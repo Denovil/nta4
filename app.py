0a0f6f03b20f876c751c7beb074bf43ff13b454e
Denovil <135831381+Denovil@users.noreply.github.com>
2023-06-07 08:34:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.write("My first application")
 Username = st.text_input("Enter Username")
 Age = st.number_input("Enter Age")
-if Age >= 18:
+if 18 =< Age >= 100:
   st.write("vote ")
 else:
   st.write("Can't vote ")
